50914e67b7214bf28632e6d4d4bbed5c5d46a28a
rhit-yangz4 <yangz4@rose-hulman.edu>
2021-06-23 13:15:17 +0800

    Initial Commit

---
+++ Linear Lights Out Universal App lab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Linear Lights Out Universal App lab
+//
+//  Created by Sisyphus on 6/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
